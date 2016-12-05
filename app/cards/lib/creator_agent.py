fa150a3b4bfeffb359ce92b19fbaaf94be7a07cf
Jouko <jouko.niinimaki@cs.helsinki.fi>
2016-12-05 19:57:15 +0200

    mana is drawn from rand dist and health and attack are based on mana

--- app/cards/lib/creator_agent.py
+++ app/cards/lib/creator_agent.py
@@ -1,3 +1,7 @@
+from numpy.random import normal
+from scipy.stats import truncnorm
+from math import ceil
+
 from cards.models import *
 
 
@@ -13,12 +17,13 @@ class Creator_agent:
     
     def create_card(self):
         card = {}
+        card_val = 0
 
-        card['type'] = "Minion"
-        card['mana'] = 3
+        
+        card['mana'] = int(round(normal(2.5, 1.5)))
         card['mechanics'] = [("Shit mechanic", 123)]
-        card['health'] = 1
-        card['attack'] = 2
+        
+        card_val += self._set_healt_and_attack(card, card["mana"])
         
         card['name'] = "Teemo's Mushroom"
         card['rarity'] = "Epic" #Rarity.objects.order_by('?').first().name
@@ -26,10 +31,44 @@ class Creator_agent:
         
         card['player_class'] = "All"
         
-        card['value'] = -100
+        
         card['image'] = "http://i.imgur.com/TqFP5w7.jpg"
         
+        card['value'] = card_val
+        card['type'] = "Minion"
         return card
+    
+    
+    def _set_healt_and_attack(self, card, mana):
+        '''Generates health and attack for a card based on given mana. Mana
+        is divided for health and attack by using truncated normal
+        distribution.
+
+        :param dict card: card to set the values to
+        :param int mana: total mana the values should be based on
+        :returns:
+            combined value of the attack and health generated
+        '''
+        
+        #mana values for single attack and health points
+        health_val = MetaData.objects.get(name='health_coeff').value
+        attack_val = MetaData.objects.get(name='minion_attack_coeff').value
+        print(health_val)
+        print(attack_val)
+        
+        #random value between 0 and 1 using truncated normal distribution
+        #with mean 0.5 and standard deviation 0.2
+        rnd = truncnorm(-0.5/0.2, 0.5/0.2, loc=0.5, scale=0.2).rvs()
+        
+        health_val_target = rnd * mana
+        attack_val_target = (1 - rnd) * mana
+        print(health_val_target)
+        print(attack_val_target)
+        
+        card['health'] = int(round(health_val_target/health_val))
+        card['attack'] = int(round(attack_val_target/attack_val))
+        
+        return card['health'] * health_val + card['attack'] * attack_val
 
 
 if __name__ == "__main__":
